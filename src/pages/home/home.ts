db02bfbf60d67b34ee0e0110fa834faaa2547557
nureedaduerama <s64309010012@ptnicec.ac.th>
2022-10-26 11:19:35 +0700

    ส่งครั้งที่ 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController } from 'ionic-angular';
+import { Calendar } from '@ionic-native/calendar';
+import { AddEventPage } from '../add-event/add-event';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,86 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  date: any;
+  daysInThisMonth: any;
+  daysInLastMonth: any;
+  daysInNextMonth: any;
+  monthNames: string[];
+  currentMonth: any;
+  currentYear: any;
+  currentDate: any;
+  eventList: any;
+  selectedEvent: any;
+  isSelected: any;
+
+  constructor(public navCtrl: NavController,
+              private calender: Calendar,
+              private alertCtrl: AlertController  
+    ) {
 
   }
 
+  ionViewWillEnter(){
+    this.date = new Date();
+    this.monthNames = [
+      "January","Febuary","March","April","May","June",
+      "July","August","September","October","December"
+    ];
+    this.getDaysOfMonth();
+    this.loadEventThisMonth();
+  }
+
+  getDaysOfMonth() {
+    this.daysInThisMonth = new Array();
+    this.daysInLastMonth = new Array();
+    this.daysInNextMonth = new Array();
+    this.currentMonth = this.monthNames[this.date.getMonth()];
+    this.currentYear = this.date.getFullYear();
+    if(this.date.getMonth() === new Date().getMonth()) {
+      this.currentDate = new Date().getDate();
+    } else {
+      this.currentDate = 999;
+    }
+  
+    var firstDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 1).getDay();
+    var prevNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth(), 0).getDate();
+    for(var i = prevNumOfDays-(firstDayThisMonth-1); i <= prevNumOfDays; i++) {
+      this.daysInLastMonth.push(i);
+    }
+  
+    var thisNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth()+1, 0).getDate();
+    for (var j = 0; j < thisNumOfDays; j++) {
+      this.daysInThisMonth.push(j+1);
+    }
+  
+    var lastDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth()+1, 0).getDay();
+    //var nextNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth()+2, 0).getDate();
+    for (var k = 0; k < (6-lastDayThisMonth); k++) {
+      this.daysInNextMonth.push(k+1);
+    }
+    var totalDays = this.daysInLastMonth.length+this.daysInThisMonth.length+this.daysInNextMonth.length;
+    if(totalDays<36) {
+      for(var l = (7-lastDayThisMonth); l < ((7-lastDayThisMonth)+7); l++) {
+        this.daysInNextMonth.push(l);
+      }
+    }
+  }
+
+  goToLastMonth() {
+    this.date = new Date(this.date.getFullYear(), this.date.getMonth(), 0);
+    this.getDaysOfMonth();
+  }
+
+  goToNextMonth() {
+    this.date = new Date(this.date.getFullYear(), this.date.getMonth()+2, 0);
+    this.getDaysOfMonth();
+  }
+
+  loadEventThisMonth(){
+
+  }
+
+  addEvent() {
+    this.navCtrl.push(AddEventPage);
+  }
 }
